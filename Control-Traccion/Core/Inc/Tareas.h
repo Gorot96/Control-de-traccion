0f1913e09bebf2cc7000a5b76326596c72f192d6
raudurgar <raudurgar@alum.us.es>
2023-03-08 12:15:24 +0100

    Creado programa de gestión de tareas

---
+++ Control-Traccion/Core/Inc/Tareas.h
@@ -0,0 +1,17 @@
+/*
+ * Tareas.h
+ *
+ *  Created on: Mar 8, 2023
+ *      Author: Raul Duran
+ */
+
+#include "main.h"
+#include "cmsis_os.h"
+#include "FreeRTOS.h"
+
+#ifndef INC_TAREAS_H_
+#define INC_TAREAS_H_
+
+
+
+#endif /* INC_TAREAS_H_ */
---
+++ Control-Traccion/Core/Src/Tareas.c
@@ -0,0 +1,9 @@
+/*
+ * Tareas.c
+ *
+ *  Created on: Mar 8, 2023
+ *      Author: Raul Duran
+ */
+
+
+#include "Tareas.h"
