14db279d5c48efdaef3c1054e6fb92ce26fe5296
Rauldr37 <rauldurangarcia2001@gmail.com>
2023-04-14 09:37:38 +0200

    Hasta aquí me ha podido ayudar Pablo digo ChatGPT

--- Python/obtain_data.py
+++ Python/obtain_data.py
@@ -1,9 +1,15 @@
 import requests
 import json
 import time
+import requests
 
 # Set the URL of the JSON file
 micro_url = "http://192.168.106.124"
+serv_url = 'http://127.0.0.1:8000/guardar_estado_boton/'
+
+data = {
+    'estado_boton': 'presionado'
+}
 
 # This function obtains a JSON from a error message 
 # (because the wepbage is headerless, so it thinks that the header is actually the JSON from the webpage).
@@ -27,9 +33,18 @@ def serialize_data(json):
         for p in o.values():
             print(p)
 
+def get_server_data(url):
+    response = requests.get(url)
+    print(response.text)  # imprime el contenido de la respuesta
+    data = response.json()
+    estado_boton = data['estado_boton']
+    print(f'El estado actual del botón es: {estado_boton}')
+
 # Main program
 while (True):
-    data = get_json(micro_url)
-    serialize_data(data)
+    #data = get_json(micro_url)
+    #serialize_data(data)
+
+    get_server_data(serv_url)
 
     time.sleep(0.25)
